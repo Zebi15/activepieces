23ec66056e5e0fd5c9590d520550e7086d8e9b25
Zebi15 <eusebiuapostol@gmail.com>
2025-05-03 13:49:05 +0300

    feat(2solar): add common utilities for integration

---
+++ packages/pieces/community/2solar/src/lib/common/index.ts
@@ -0,0 +1,9 @@
+export const TwoSolarCommon = {
+    baseUrl: 'https://app.2solar.nl/api',
+    
+    // API endpoints
+    endpoints: {
+      searchPersons: '/person/search/',
+      updatePerson: '/person/{id}/'
+    }
+  };
